62f7732fc407cf7b597d1b58675a001787ab78d4
Kabir Tamari <rabbit.bonex@gmail.com>
2019-04-06 15:36:48 +0545

    Database finilized for now/ will be working on logics

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Order, OrderedItem, CustomUser, FoodItem
+from .models import Order, OrderedItem, CustomUser, FoodItem, FoodType
 # Register your models here.
 
 admin.site.register(Order)
 admin.site.register(OrderedItem)
 admin.site.register(CustomUser)
 admin.site.register(FoodItem)
+admin.site.register(FoodType)
--- api/models.py
+++ api/models.py
@@ -2,19 +2,58 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 import uuid
-from django.db.models.signals import post_save
+# from django.db.models.signals import post_save
 # from .consumers import StaffConsumer
 
 class CustomUser(AbstractUser):
     is_manager = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=True)
+    is_table = models.BooleanField(default = False)
     uuid = models.UUIDField(unique = True,default = uuid.uuid4)
+
+class Table (models.Model):
+    REQUIRED_FIELDS = ('user',)
+    user = models.OneToOneField(CustomUser, on_delete = models.CASCADE, primary_key = True, related_name = "Table", unique = True)
+    table_number = models.PositiveIntegerField()
     
+    def save(self, *args, **kwargs):
+        self.user.is_manager = False
+        self.user.is_staff = False
+        self.user.is_table = True
+        super().save(*args, **kwargs)
+
+class Supervisor(CustomUser):
+    REQUIRED_FIELDS = ('user',)
+    user = models.OneToOneField(CustomUser, on_delete = models.CASCADE, primary_key = True, related_name = "Supervisor", unique = True)
+    contact_number = models.CharField(blank = True, max_length  =  14)
+
+    def save(self, *args, **kwargs):
+        self.user.is_manager = True
+        self.user.is_staff = True
+        self.user.is_table = False
+        super().save(*args, **kwargs)
+
+
+class Staff(CustomUser):
+    REQUIRED_FIELDS = ('user',)
+    user = models.OneToOneField(CustomUser, on_delete = models.CASCADE, primary_key = True, related_name = "Staff", unique = True)
+    contact_number = models.CharField(blank = True, max_length  =  14)
+
+    def save(self, *args, **kwargs):
+        self.user.is_manager = False
+        self.user.is_staff = True
+        self.user.is_table = False
+        super().save(*args, **kwargs)
+
+class FoodType(models.Model):
+    food_type = models.CharField(max_length = 200,null = True)
+
+
 class FoodItem(models.Model):
     name = models.CharField(max_length = 200)
+    food_type = models.ForeignKey(FoodType, on_delete = models.CASCADE,null = True)
     price = models.PositiveIntegerField(default = 100)
     is_active = models.BooleanField(default= True)
-    code = models.PositiveIntegerField(unique = True,null = True)
 
     def __str__(self):
         return self.name
@@ -35,8 +74,8 @@ class OrderedItem(models.Model):
     food_item = models.ForeignKey(FoodItem, on_delete = models.CASCADE)
     quantity = models.IntegerField()
 
-def send_ordered_item (sender, instance,**kwargs):
-    print(instance)
-    print("this")
+# def send_ordered_item (sender, instance,**kwargs):
+#     print(instance)
+#     print("this")
 
-post_save.connect(send_ordered_item, sender=OrderedItem)
+# post_save.connect(send_ordered_item, sender=OrderedItem)
